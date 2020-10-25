b1df8e9392aaa0b6cbd7bf46796b61e70c9c13b0
Amadou <amadou_g@hotmail.fr>
2020-10-25 12:57:43 +0100

    Adding Fragment Navigation (Map -> Login -> Sign Up) Adding Map Detail Card Adding English Support
    Signed-off-by: Amadou <amadou_g@hotmail.fr>

---
+++ app/src/main/java/com/example/myf_zone/fragments/SignUpFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myf_zone.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myf_zone.R
+
+class SignUpFragment : Fragment() {
+    //    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.become_coach)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.login_coach)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+}
